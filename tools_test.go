bdaeab6296fd2a1c5dbecd4c275c1747d8d10813
Vyshnav M <vyshnavmeleppat@gmail.com>
2024-09-20 11:58:54 +0530

    changed expectedLength in random string test to 20

--- tools_test.go
+++ tools_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestTools_RandomString(t *testing.T) {
 	var testTools Tools
 
-	expectedLength := 10
+	expectedLength := 20
 	s := testTools.RandomString(expectedLength)
 	if len(s) != expectedLength {
 		t.Errorf("wrong length random string returned, expected length %d but got length %d", expectedLength, len(s))
